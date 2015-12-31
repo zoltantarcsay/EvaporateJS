a30ff2488a7b5c03501c3660d35ea67dd8c54fed
Bobby Wallace <bikeath1337@me.com>
2015-12-31 06:37:41 -0500

    Bumps version to 0.0.9.

--- evaporate.js
+++ evaporate.js
@@ -14,7 +14,7 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
 
 /***************************************************************************************************
 *                                                                                                  *
-*  version 0.0.8                                                                                  *
+*  version 0.0.9                                                                                  *
 *                                                                                                  *
 *  TODO:                                                                                           *
 *       post eTags to application server to allow resumability after client-side crash/restart      *
